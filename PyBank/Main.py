9a2239fb3cf7a4d0de147e49f6c7b271d1f1d2e1
Dave Borowski <dmborowski@msn.com>
2020-06-27 17:45:07 -0600

    Added code tp calculate totals and create new lists for further calculations

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -12,6 +12,7 @@ with open(csvpath) as budget_file:
 
     # Set the budget header to leave out of calculations
     budget_header = next(budget)
+    # print(f'Budget Header: {budget_header}')
 
     # Define new lists and set values to zero
     num_mos = 0
@@ -22,4 +23,18 @@ with open(csvpath) as budget_file:
     yr_pl = [] # individual year profit/loss
     yr = [] # month and year
     delta = [] # YoY change
+
+    # Running calculations by row while reading the csv
+    for row in budget:
+        num_mos = num_mos + 1 # Counts the number of months in the budget file
+        total = total + int(row[1]) # Adds the current month budget at index = 1 to the running total
+        yr_pl.append(row[1]) # appends the current profit/loss value into a list for difference and average calculations
+        yr.append(row[0]) # appends the current month into a list for reference for the max/min months
+        prev = yr_pl[0] # Assigns the first year profit/loss to prev for difference calculation
+
+    print(num_mos)
+    print(total)
+    print(yr_pl)
+    print(prev)
+
 
