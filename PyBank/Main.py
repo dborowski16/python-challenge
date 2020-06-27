5e573d94f0465a8cbf127681db2447adf94b3d54
Dave Borowski <dmborowski@msn.com>
2020-06-27 17:32:54 -0600

    Updated code for PyBank...Read in csv and defined variables and strings

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,1 +1,25 @@
+# Import need modules for import csv
+import os
+import csv
+
+# Define the csv path
+csvpath = os.path.join('..','PyBank\Resources', 'budget_data.csv')
+print(csvpath)
+
+# Open the csv w/ comma as delimiter
+with open(csvpath) as budget_file:
+    budget = csv.reader(budget_file, delimiter = ',')
+
+    # Set the budget header to leave out of calculations
+    budget_header = next(budget)
+
+    # Define new lists and set values to zero
+    num_mos = 0
+    total = 0
+    avg_delta = 0
+    max_delta = 0
+    min_delta = 0
+    yr_pl = [] # individual year profit/loss
+    yr = [] # month and year
+    delta = [] # YoY change
 
