8cef943425609dc8abd4854d1bfadc85042ab51b
Dave Borowski <dmborowski@msn.com>
2020-06-27 18:11:28 -0600

    Added print statements to create summary

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -21,7 +21,7 @@ with open(csvpath) as budget_file:
     max_delta = 0
     min_delta = 0
     mo_pl = [] # individual month profit/loss
-    mo = [] # month and year
+    mo = [] # month and month
     delta = [] # MoM change
 
     # Running calculations by row while reading the csv
@@ -30,19 +30,20 @@ with open(csvpath) as budget_file:
         total = total + int(row[1]) # Adds the current month budget at index = 1 to the running total
         mo_pl.append(row[1]) # appends the current profit/loss value into a list for difference and average calculations
         mo.append(row[0]) # appends the current month into a list for reference for the max/min months
-        prev = mo_pl[0] # Assigns the first year profit/loss to prev for difference calculation
+        prev = mo_pl[0] # Assigns the first month profit/loss to prev for difference calculation
 
     # For loop to calculate the MoM deltas, average delta, and check for max and min MoM change
     for i in range(1,86):
         change = int(mo_pl[i]) - int(prev) # MoM change 
         delta.append(change) # Creates a new list containing the MoM change
-        prev = mo_pl[i] # Sets the previous year value to the current year
+        prev = mo_pl[i] # Sets the previous month value to the current month
         avg_delta = avg_delta + change # Calculates total of the MoM changes, to be used to calculate an average MoM later
 
         # Check to see it the current MoM change is greater than or less than the current values
         if int(delta[i-1]) > max_delta:
             max_delta = delta[i-1]
             max_month = mo[i]
+
         elif int(delta[i-1]) < min_delta:
             min_delta = delta[i-1]
             min_month = mo[i]
@@ -50,13 +51,13 @@ with open(csvpath) as budget_file:
     # calculate average MoM change using the total months
     average = avg_delta/(num_mos - 1)
 
-
-    print(num_mos)
-    print(total)
-    print(max_delta)
-    print(max_month)
-    print(min_delta)
-    print(min_month)
-    print(average)
+# Creating a summary table
+print('Financial Analysis')
+print('-'*30)
+print(f'Total Months: {num_mos}')
+print(f'Total : ${total}')
+print(f'Average Change: ${average}')
+print(f'Greatest Increase in Profits: {max_month} (${max_delta})')
+print(f'Greatest Decrease in Profits: {min_month} (${min_delta})')
 
 
