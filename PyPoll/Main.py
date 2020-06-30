7211ee3514ad5fc6fe1e3d2cf12e373e1b664f9e
Dave Borowski <dmborowski@msn.com>
2020-06-30 16:40:39 -0600

    Added code to create a new list for unique candidate names based upon the votes and create a loop that totals the votes for each candidate into a totals list

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -7,27 +7,47 @@ thisFolder = os.path.dirname(os.path.abspath(__file__))
 csvpath = os.path.join(thisFolder,'Resources', 'election_data.csv')
 print(csvpath)
 
+# Open csv file with comma as delimiter
 with open(csvpath) as election_file:
     election = csv.reader(election_file, delimiter = ',')
 
+    # Set header to ignore first row of data
     election_header = next(election)
     # print(f'Election Header: {election_header}')
 
+    # Define variables and new lists
     total_votes = 0
-    candidates = []
-    candidate =[]
-    candidate_total = []
+    vote = []
+    candidate = []
+    candidate_total = [0, 0, 0, 0]
     county = []
 
+    # for loop through rows to calculate total votes and create new list with candidates and counties
     for row in election:
         total_votes = total_votes + 1
-        candidates.append(row[2])
+        vote.append(row[2])
         county.append(row[1])
+    # print(total_votes)
+    # print(len(vote))
 
-    for name in candidates:
+    # for loop to create a new list with unique candidate names
+    for name in vote:
         if name not in candidate:
             candidate.append(name)
-    print(candidate)
-             
-    # print(candidates)
+    # print(vote[0])
+    # print(candidate)
+
+    # for loop to calculate number of votes per candidate
+    for i in range(1, (total_votes)):
+        if vote[i-1] == candidate[0]:
+            candidate_total[0] = candidate_total[0] + 1
+        elif vote[i-1] == candidate[1]:
+            candidate_total[1] = candidate_total[1] + 1  
+        elif vote[i-1] == candidate[2]:
+            candidate_total[2] = candidate_total[2] + 1
+        elif vote[i-1] == candidate[3]:
+            candidate_total[3] = candidate_total[3] + 1
+
+    print(candidate_total)
+
 
